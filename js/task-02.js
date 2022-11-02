4d2454f15326ba05b4087e1d119c67d1aae1e130
Markov Dmitriy Yurevich <d.markov@burdaua.vhbk>
2022-11-02 23:36:11 +0200

    сделаны 7 заданий

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,16 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ["Potatoes", "Mushrooms", "Garlic", "Tomatos", "Herbs", "Condiments"];
+
+const listOfIngredient = document.querySelector(`#ingredients`);
+console.log(listOfIngredient);
+
+for (const ingredient of ingredients) {
+	const liEl = document.createElement("li");
+	liEl.textContent = `${ingredient}`;
+	liEl.classList.add(`item`);
+
+	console.log(liEl);
+
+	listOfIngredient.append(liEl);
+}
+
+console.log(listOfIngredient);
---
+++ js/task-06.js
@@ -0,0 +1,22 @@
+const refs = {
+	inputField: document.querySelector(`#validation-input`),
+};
+
+const inputHandler = event => {
+	const limitOfDigits = Number(refs.inputField.getAttribute("data-length"));
+
+	if (event.target.value.length === limitOfDigits) {
+		refs.inputField.classList.add(`valid`);
+	} else {
+		refs.inputField.classList.add(`invalid`);
+	}
+};
+
+const focusHandler = event => {
+	event.target.value = ``;
+	refs.inputField.classList.remove(`valid`);
+	refs.inputField.classList.remove(`invalid`);
+};
+
+refs.inputField.addEventListener(`blur`, inputHandler);
+refs.inputField.addEventListener(`focus`, focusHandler);
