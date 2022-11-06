5e3578bff67a3bedc544635dd23b1a6f39fa8a3f
Markov Dmitriy Yurevich <d.markov@burdaua.vhbk>
2022-11-06 17:33:21 +0200

    tasks-01-09

--- js/task-10.js
+++ js/task-10.js
@@ -1,17 +1,3 @@
 function getRandomHexColor() {
 	return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
-
-const refs = {
-	input: document.querySelector(`#controls > input`),
-};
-
-let numberOfBoxes = 1;
-
-const inputHandle = event => {
-	numberOfBoxes = event.target.value;
-	console.log(numberOfBoxes);
-};
-
-refs.input.addEventListener(`change`, inputHandle);
-console.log(numberOfBoxes);
