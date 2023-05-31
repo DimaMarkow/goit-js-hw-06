a0b11b5dbf8e3a7b8d8da4d7e735e65bfe6c46ac
Markov Dmitriy Yurevich <d.markov@burdamedia.vhbk>
2023-05-31 21:27:58 +0300

    fix red border

--- js/task-06.js
+++ js/task-06.js
@@ -5,6 +5,13 @@ const refs = {
 const inputHandler = event => {
 	const limitOfDigits = Number(refs.inputField.getAttribute("data-length"));
 
+	console.log(limitOfDigits);
+	console.log(event.target.value.length);
+
+	if (refs.inputField.classList.contains(`invalid`)) {
+		refs.inputField.classList.remove(`invalid`);
+	}
+
 	if (event.target.value.length === limitOfDigits) {
 		refs.inputField.classList.add(`valid`);
 	} else {
